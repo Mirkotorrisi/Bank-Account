001d1d9422cc2d6827d2d4f8b01361cec26c3f1d
Mirkotorrisi <52355747+Mirkotorrisi@users.noreply.github.com>
2019-07-24 15:12:02 +0200

    Update Checking.java

--- Checking.java
+++ Checking.java
@@ -1,21 +1,28 @@
 package bankaccount;
 
 public class Checking extends Account {
-	int debitCardNumber;
-	int debitCardPin;
-	public Checking(String name, String ssn, double initialDeposit) {
-		super(name,ssn,initialDeposit);
+	private int debitCardNumber;
+	private int debitCardPin;
+	public Checking(String name, String ssn, double initialDeposit, String dateStart) {
+		super(name,ssn,initialDeposit,dateStart);
+		accountType = "Checking";
 		accountNumber = "2" + accountNumber;
 		setDebitCard();
+	}
+	@Override
+	public void setRate() {
+		rate = getBaseRate()*.15;
 	}
 	private void setDebitCard() {
 		debitCardNumber = (int)(Math.random()*Math.pow(10, 12));
 		debitCardPin = (int)(Math.random()*Math.pow(10, 4));
-		System.out.println("Card: "+this.debitCardNumber+"\nPIN: "+this.debitCardPin);
 	}
 		
 	public void showInfo() {
-		super.showInfo();
 		System.out.println("Checking account");
+		super.showInfo();
+		System.out.println("Card: "+this.debitCardNumber+"\nPIN: "+this.debitCardPin);
+
+
 	}
 }
